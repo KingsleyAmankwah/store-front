4d2c158817c81c7d753ade9c4b77001563c7c418
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2023-12-11 09:36:29 -0800

    routing for login, register and unknown page done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { RegisterComponent } from './components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home' },
+  { path: 'login', component: AuthComponent, title: 'Login' },
+  { path: 'register', component: RegisterComponent, title: 'Register' },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
