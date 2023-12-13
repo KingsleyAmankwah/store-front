aaba86ef26b5ac46eb2b2c63ef2d3e461073eec8
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2023-12-13 10:50:17 -0800

    feat/ business class refactored

--- src/app/components/business-sign-up/business-sign-up.component.ts
+++ src/app/components/business-sign-up/business-sign-up.component.ts
@@ -1,5 +1,5 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -7,6 +7,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { CustomValidators } from '../custom-validators';
 
 @Component({
   selector: 'app-business-sign-up',
@@ -15,34 +16,33 @@ import { RouterLink } from '@angular/router';
   templateUrl: './business-sign-up.component.html',
   styleUrl: './business-sign-up.component.css',
 })
-export class BusinessSignUpComponent {
+export class BusinessSignUpComponent implements OnInit {
   registerForm!: FormGroup;
+  showPassword = false;
+  showConfirmPassword = false;
   errorMessage = '';
 
   constructor(private fb: FormBuilder) {}
 
-  handleRegister() {
+  ngOnInit() {
+    this.handleRegisterationForm();
+  }
+
+  handleRegisterationForm() {
     this.registerForm = this.fb.group({
       name: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
-      confirmPassword: ['', [Validators.required]],
+      password: [
+        '',
+        [Validators.required, CustomValidators.passwordStrength()],
+      ],
+      confirmPassword: [
+        '',
+        [Validators.required, CustomValidators.passwordStrength()],
+      ],
     });
   }
 
-  passwordsMatch() {
-    return (
-      this.confirmPassword?.touched &&
-      this.password?.value === this.confirmPassword?.value
-    );
-  }
-
-  passwordsMismatch() {
-    return (
-      this.registerForm.get('confirmPassword')?.touched &&
-      this.password?.value !== this.confirmPassword?.value
-    );
-  }
   get name() {
     return this.registerForm.get('name');
   }
@@ -58,8 +58,35 @@ export class BusinessSignUpComponent {
     return this.registerForm.get('confirmPassword');
   }
 
+  passwordsMatch() {
+    return (
+      this.confirmPassword?.touched &&
+      this.password?.value === this.confirmPassword?.value
+    );
+  }
+
+  passwordsMismatch() {
+    return (
+      this.registerForm.get('confirmPassword')?.touched &&
+      this.password?.value !== this.confirmPassword?.value
+    );
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPasswordVisibility() {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
   showErrorMessage(): boolean {
-    if (this.email && this.email.invalid && this.email.touched) {
+    if (this.name && this.name.invalid && this.name.touched) {
+      this.errorMessage = this.name.errors?.['required']
+        ? 'Business name is required!'
+        : '';
+      return true;
+    } else if (this.email && this.email.invalid && this.email.touched) {
       this.errorMessage = this.email.errors?.['required']
         ? 'Email is required!'
         : this.email.errors?.['email']
@@ -71,7 +98,7 @@ export class BusinessSignUpComponent {
       return true;
     }
 
-    this.errorMessage = ''; // Reset the error message if no errors
+    this.errorMessage = '';
     return false;
   }
 }
