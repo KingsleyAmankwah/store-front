564f5638cca51880be16c6b09a25566983528dad
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2023-12-12 16:24:28 -0800

    routes for business and individual account added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,21 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AuthComponent } from './components/auth/auth.component';
-import { RegisterComponent } from './components/register/register.component';
+import { BusinessSignUpComponent } from './components/business-sign-up/business-sign-up.component';
+import { IndividualSignUpComponent } from './components/individual-sign-up/individual-sign-up.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home' },
   { path: 'login', component: AuthComponent, title: 'Login' },
-  { path: 'register', component: RegisterComponent, title: 'Register' },
+  {
+    path: 'sign-up',
+    component: BusinessSignUpComponent,
+    title: 'Business Account',
+  },
+  {
+    path: 'register',
+    component: IndividualSignUpComponent,
+    title: 'Individual Account',
+  },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
