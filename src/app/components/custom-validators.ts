282215f16596a9d1e3c27e332198d3e0fee188da
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2023-12-13 07:14:34 -0800

    custom validators

---
+++ src/app/components/custom-validators.ts
@@ -0,0 +1,23 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export class CustomValidators {
+  static passwordStrength() {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (!value) {
+        return null;
+      }
+      const hasUpperCase = /[A-Z]+/.test(value);
+      const hasLowerCase = /[a-z]+/.test(value);
+      const hasNumeric = /[0-9]+/.test(value);
+      const hasSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(value);
+      const passwordValid =
+        hasUpperCase &&
+        hasLowerCase &&
+        hasNumeric &&
+        hasSpecial &&
+        value.length >= 8;
+      return !passwordValid ? { passwordStrength: true } : null;
+    };
+  }
+}
