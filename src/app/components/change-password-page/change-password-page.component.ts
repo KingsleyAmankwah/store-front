5bfcc69edd616b1876aa4e186895d6209aee374f
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2023-12-15 09:04:17 -0800

    change password component

--- src/app/components/change-password-page/change-password-page.component.ts
+++ src/app/components/change-password-page/change-password-page.component.ts
@@ -1,12 +1,71 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CustomValidators } from '../custom-validators';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-change-password-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './change-password-page.component.html',
-  styleUrl: './change-password-page.component.css'
+  styleUrl: './change-password-page.component.css',
 })
 export class ChangePasswordPageComponent {
+  changePasswordForm!: FormGroup;
+  errorMessage = '';
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.changePasswordForm = this.fb.group({
+      password: [
+        '',
+        [Validators.required, CustomValidators.passwordStrength()],
+      ],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  get password() {
+    return this.changePasswordForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.changePasswordForm.get('confirmPassword');
+  }
+
+  passwordsMatch() {
+    return (
+      this.confirmPassword?.touched &&
+      this.password?.value === this.confirmPassword?.value
+    );
+  }
+
+  passwordsMismatch() {
+    return (
+      this.changePasswordForm.get('confirmPassword')?.touched &&
+      this.password?.value !== this.confirmPassword?.value
+    );
+  }
+
+  showErrorMessage(): boolean {
+    if (this.passwordsMismatch()) {
+      this.errorMessage = 'The two passwords do not match!';
+      return true;
+    } else {
+      this.errorMessage = '';
+      return false;
+    }
+  }
+
+  changePassword() {
+    if (this.changePasswordForm.valid) {
+      console.log(`Hello world`);
+    }
+  }
 }
