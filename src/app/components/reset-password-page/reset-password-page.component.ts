1a205bf09aaea3468c6c8f24a4704713781a9b22
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2023-12-14 16:29:10 -0800

    reset password component

---
+++ src/app/components/reset-password-page/reset-password-page.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-reset-password-page',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './reset-password-page.component.html',
+  styleUrl: './reset-password-page.component.css',
+})
+export class ResetPasswordPageComponent implements OnInit {
+  resetPasswordForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.resetPasswordForm = this.fb.group({
+      email: ['', Validators.required, Validators.email],
+    });
+  }
+
+  get email() {
+    return this.resetPasswordForm.get('email');
+  }
+
+  sendMail() {
+    console.log(`Hello World!`);
+  }
+}
