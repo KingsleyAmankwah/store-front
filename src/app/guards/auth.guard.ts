68cee3ebbf54ebb2c5eafb3cbbddc8832e830f05
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2023-12-20 08:50:52 -0800

    🔥:feat/ auth guard to  protect the routes

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,37 @@
+//?  In this revised code, sessionStorage is accessed only if the platform is a browser.
+//?  This is also done to prevent the server-side rendering from accessing sessionStorage.
+//? This is because sessionStorage is not available on the server-side.
+//?  Causing ReferenceError: sessionStorage is not defined.
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const platformId = inject(PLATFORM_ID);
+  const router = inject(Router);
+
+  if (isPlatformBrowser(platformId)) {
+    const userEmail = sessionStorage.getItem('userEmail');
+
+    if (!userEmail) {
+      return router.createUrlTree(['/register']);
+    } else {
+      return true;
+    }
+  } else {
+    // Handle server-side rendering case or redirect as appropriate
+    return router.createUrlTree(['/register']);
+  }
+};
+
+// export const authGuard: CanActivateFn = (route, state) => {
+//   const router = inject(Router);
+//   const userEmail = sessionStorage.getItem('userEmail');
+
+//   if (!userEmail) {
+//     return router.createUrlTree(['/register']);
+//   } else {
+//     return true;
+//   }
+// };
