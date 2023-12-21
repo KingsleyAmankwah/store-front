7c9182024820f21322a265ada3011a287bd66ea9
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2023-12-21 15:00:02 -0800

    main dashboard content

---
+++ src/app/components/dashboard-main-content/dashboard-main-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-main-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-main-content.component.html',
+  styleUrl: './dashboard-main-content.component.css'
+})
+export class DashboardMainContentComponent {
+
+}
