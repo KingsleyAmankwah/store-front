9cdb347f855f710ca9889c92152ab30334daa7e0
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2023-12-21 15:03:01 -0800

    dashboard component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
+import { ProfileComponent } from '../profile/profile.component';
+import { SettingsComponent } from '../settings/settings.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    SidebarComponent,
+    ProfileComponent,
+    SettingsComponent,
+    RouterOutlet,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
