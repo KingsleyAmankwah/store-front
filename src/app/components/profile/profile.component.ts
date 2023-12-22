59d219fbb423a3237c96d81f79c374b9e9c5fc6a
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2023-12-22 14:59:09 -0800

    profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,20 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
+  userName!: string;
+  userImage!: string;
 
+  ngOnInit() {
+    const userData = JSON.parse(sessionStorage.getItem('userData') || '{}');
+    this.userName = userData.name;
+    this.userImage = userData.photo;
+  }
 }
